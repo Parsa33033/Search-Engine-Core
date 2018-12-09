321dd1a19fa3c7ee62429a51bd8791f346fbe9c2
Hassan Farhikhteh <h.farhikhteh@gmail.com>
2018-12-09 08:28:21 +0330

    Update Query.java , add SortByValue

--- src/main/java/searchengine/Query.java
+++ src/main/java/searchengine/Query.java
@@ -113,7 +113,26 @@ public class Query implements IndexerInterface, Runnable{
 	 * @return
 	 */
 	public Map<String, Double> sortByValue(Map<String, Double> unsortMap) {
+        // Convert Map to List of Map
+        List<Map.Entry<String, Double>> list =
+                new LinkedList<Map.Entry<String, Double>>(unsortMap.entrySet());
 
+        // Sort list with Collections.sort(), provide a custom Comparator
+        // Try switch the o1 o2 position for a different order
+        Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+            public int compare(Map.Entry<String, Double> o1,
+                               Map.Entry<String, Double> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        // Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+        Map<String, Double> sortedMap = new LinkedHashMap<String, Double>();
+        for (Map.Entry<String, Double> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
     }
 	
 	/**
